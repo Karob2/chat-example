2dcc94800ce3446d16edce8046417736c960c4c7
Karob2 <krichotomy@gmail.com>
2018-07-16 17:04:43 -0500

    disconnect messages

--- index.js
+++ index.js
@@ -19,6 +19,24 @@ io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
     });
+    socket.on('disconnect', function(reason){
+        socket.broadcast.emit('system message', "someone disconnect: " + reason);
+        socket.emit('system message', "you disconnect: " + reason);
+        var clientList = Object.keys(io.sockets.sockets);
+        io.emit('system message', "population: " + clientList.length);
+    });
+    socket.on('disconnecting', function(reason){
+        socket.broadcast.emit('system message', "someone disconnecting: " + reason);
+        socket.emit('system message', "you disconnecting: " + reason);
+        var clientList = Object.keys(io.sockets.sockets);
+        io.emit('system message', "population: " + clientList.length);
+    });
+});
+io.on('disconnect', function(reason){
+    io.emit('system message', "server disconnect: " + reason);
+});
+io.on('disconnecting', function(reason){
+    io.emit('system message', "server disconnecting: " + reason);
 });
 
 http.listen(port, function(){
