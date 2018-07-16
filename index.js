536d0c06273e93849f42b88f1a8ef70267ebacd1
Karob2 <krichotomy@gmail.com>
2018-07-16 16:18:43 -0500

    added system messages

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var version = 1.1;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  socket.emit('system message', "connection established");
+  socket.emit('system message', "server build " + version);
+  socket.broadcast.emit('system message', "someone has joined the server");
+  io.emit('system message', "population: " + io.clients.length);
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
