d462cccd7176528826d29e08a01edda04f65723f
Karob2 <krichotomy@gmail.com>
2018-07-16 16:42:24 -0500

    population

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-var version = 1.1;
+var version = 1.2;
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.emit('system message', "connection established");
-  socket.emit('system message', "server build " + version);
-  socket.broadcast.emit('system message', "someone has joined the server");
-  io.emit('system message', "population: " + io.clients.length);
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+    socket.emit('system message', "connection established");
+    socket.emit('system message', "server build " + version);
+    socket.broadcast.emit('system message', "someone has joined the server");
+    var clientList;
+    io.clients((error, clients) => {
+        if (error) throw error;
+        clientList = clients;
+    });
+    io.emit('system message', "population: " + clientList.length);
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
 });
 
 http.listen(port, function(){
-  console.log('listening on *:' + port);
+    console.log('listening on *:' + port);
 });
