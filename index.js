7a2fee01c84f9bcb2acb748f347442d6cc2cdbc1
Karob2 <krichotomy@gmail.com>
2018-07-16 22:06:00 -0500

    signaling for p2p

--- index.js
+++ index.js
@@ -18,6 +18,20 @@ io.on('connection', function(socket){
     var clientList = Object.keys(io.sockets.sockets);
     io.emit('system message', 'population: ' + clientList.length);
 
+    setupListeners(socket);
+});
+io.on('disconnect', function(reason){
+    io.emit('system message', 'server disconnect: ' + reason);
+});
+io.on('disconnecting', function(reason){
+    io.emit('system message', 'server disconnecting: ' + reason);
+});
+
+http.listen(port, function(){
+    console.log('listening on *:' + port);
+});
+
+function setupListeners(socket) {
     socket.on('chat message', function(msg){
         io.emit('chat message', socket.id + ": " + msg);
     });
@@ -31,17 +45,20 @@ io.on('connection', function(socket){
         socket.broadcast.emit('system message', socket.id + ' is disconnecting: ' + reason);
         socket.emit('system message', 'you are disconnecting: ' + reason);
     });
-    socket.on('call', function(peer, desc){
-        io.sockets.sockets[peer].emit('system message', socket.id + " wants to connect with you");
-    });
-});
-io.on('disconnect', function(reason){
-    io.emit('system message', 'server disconnect: ' + reason);
-});
-io.on('disconnecting', function(reason){
-    io.emit('system message', 'server disconnecting: ' + reason);
-});
 
-http.listen(port, function(){
-    console.log('listening on *:' + port);
-});
+    socket.custom = {};
+    socket.on('call', function(peer){
+        io.sockets.sockets[peer].emit('system message', socket.id + " wants to connect with you");
+        socket.custom.interest = peer;
+    });
+    socket.on('answer', function(peer){
+        if (io.sockets.sockets[peer].custom.interest == socket.id) {
+            io.sockets.sockets[peer].emit('system message', socket.id + " has accepted your request for connection");
+            socket.custom.interest = peer;
+            io.sockets.sockets[peer].emit('connect p2p');
+        }
+    });
+    socket.on('send signaling', function(o){
+        io.sockets.sockets[o.peerId].emit('receive signaling', o);
+    });
+}
