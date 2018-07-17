e7b64a5308c4f9e91f8b9f44a30f3fb66749c1ca
Karob2 <krichotomy@gmail.com>
2018-07-16 19:12:45 -0500

    tests

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -9,32 +11,38 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-    socket.emit('system message', "connection established");
-    socket.emit('system message', "server build " + version);
-    socket.broadcast.emit('system message', socket.handshake.address + " has joined the server");
+    socket.emit('system message', 'connection established');
+    socket.emit('system message', 'server build ' + version);
+    socket.broadcast.emit('system message', socket.handshake.address + ' has joined the server');
 
-    var clientList = Object.keys(io.sockets.sockets);
-    io.emit('system message', "population: " + clientList.length);
+    //var clientList = Object.keys(io.sockets.sockets);
+    io.emit('system message', 'population: ' + io.sockets.sockets.length;//clientList.length);
 
     socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+        io.emit('chat message', socket.id + ": " + msg);
     });
     socket.on('disconnect', function(reason){
-        socket.broadcast.emit('system message', socket.handshake.address + " has disconnected: " + reason);
-        socket.emit('system message', "you disconnected: " + reason);
+        socket.broadcast.emit('system message', socket.handshake.address + ' has disconnected: ' + reason);
+        socket.emit('system message', 'you disconnected: ' + reason);
         var clientList = Object.keys(io.sockets.sockets);
-        io.emit('system message', "population: " + clientList.length);
+        io.emit('system message', 'population: ' + clientList.length);
     });
     socket.on('disconnecting', function(reason){
-        socket.broadcast.emit('system message', socket.handshake.address + " is disconnecting: " + reason);
-        socket.emit('system message', "you are disconnecting: " + reason);
+        socket.broadcast.emit('system message', socket.handshake.address + ' is disconnecting: ' + reason);
+        socket.emit('system message', 'you are disconnecting: ' + reason);
     });
+    /*
+    socket.on('request', function(desc){
+        socket.broadcast.emit('system message', socket.handshake.address + ' is disconnecting: ' + reason);
+        socket.emit('system message', 'you are disconnecting: ' + reason);
+    });
+    */
 });
 io.on('disconnect', function(reason){
-    io.emit('system message', "server disconnect: " + reason);
+    io.emit('system message', 'server disconnect: ' + reason);
 });
 io.on('disconnecting', function(reason){
-    io.emit('system message', "server disconnecting: " + reason);
+    io.emit('system message', 'server disconnecting: ' + reason);
 });
 
 http.listen(port, function(){
